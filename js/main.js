f99d277a4845fd8bfbf4f4234479cfedf2b36588
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-05 05:44:21 +0000

    in progress task#1 day#1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,60 @@
 /* global data */
 /* exported data */
+/*
+var $entry = document.querySelector('#entry');
+console.log($entry);
+
+var $title = document.querySelector('#journal-title');
+console.log($title);
+
+var $url = document.querySelector('#journal-URL');
+console.log($url);
+
+var $notes = document.querySelector('#journal-notes');
+console.log($notes);
+
+var $img = document.querySelector('img');
+console.log($img);
+
+var $input = document.querySelectorAll('input');
+
+$title.addEventListener('focus', handleFocus);
+$title.addEventListener('input', handleInput);
+$url.addEventListener('input', handleInput);
+$url.addEventListener('focus', handleFocus);
+$notes.addEventListener('focus', handleFocus);
+$notes.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  if (event.target !== $url) {
+    console.log(event.target.value);
+  } else if (event.target === $url) {
+    $img.setAttribute('href', 'event.target.value');
+  }
+}
+
+function handleFocus(event) {
+  $input.className = 'purple-border';
+}
+
+var output = [];
+
+output += nextEntryId
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var valueTitle = $entry.elements.title.value;
+  var valuePhoto = $entry.elements.photo.value;
+  var valueNotes = $entry.elements.notes.value;
+  var data = {
+    name: valueTitle,
+    photo: valuePhoto,
+    notes: valueNotes
+  };
+  output.push(data);
+  $entry.reset();
+  return output;
+}
+
+$entry.addEventListener('submit', handleSubmit);
+*/
